2b009d40ee707f4fa950e0a61988afcedf727656
Anish Das <anishdas@mac.mynetworksettings.com>
2025-08-19 10:12:32 -0400

    build: output to docs for GH Pages branch deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/copecalm/',
+      build: {
+        outDir: 'docs',
+        emptyOutDir: true
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
