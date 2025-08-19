22fcf22628f59fea99b76db4f28d510b1d6a0b0f
Anish Das <anishdas@mac.mynetworksettings.com>
2025-08-19 10:51:09 -0400

    Add missing Vite React plugin

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App.js';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -13,5 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
